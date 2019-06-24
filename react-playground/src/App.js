261639f1bf27d4a53b3a51d60a99c738ca65f54f
ahunkele <31667935+ahunkele@users.noreply.github.com>
2019-06-24 13:41:41 -0500

    added a running state to time

--- react-playground/src/App.js
+++ react-playground/src/App.js
@@ -5,23 +5,35 @@ class App extends React.Component {
     super();
 
     this.state = {
+      isRunning: false,
       count: 0
     };
   }
 
   start = () => {
-    this.interval = setInterval(() => this.setState({ count: this.state.count + 1 }), 1000 );
+    if(this.state.isRunning === false)
+    {
+      this.interval = setInterval(() => this.setState(
+        {isRunning: true, 
+        count: this.state.count + 1 
+        }), 1000 );
+    }
   }
 
   stop = () => {
     clearInterval(this.interval);
+    this.setState({isRunning: false});
+  }
+
+  clear = () => {
+    this.setState({isRunning: false, count: 0 });
   }
 
   render() {
     return (
       <div>
         <h1> {this.state.count} </h1>
-        <button onClick={this.start}>Strat Timer!</button> <button onClick={this.stop}>Stop!</button>
+        <button onClick={this.start}>Strat Timer!</button> <button onClick={this.stop}>Stop!</button> <button onClick={this.clear}>Clear!</button>
         <br />
       </div>
     )
